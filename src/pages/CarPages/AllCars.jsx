ff9b641b90fb51b252b8eaca863b15db865f47d1
johnimeson0 <johnimeson0@gmail.com>
2022-09-02 12:36:34 +0100

    final

--- src/pages/CarPages/AllCars.jsx
+++ src/pages/CarPages/AllCars.jsx
@@ -29,9 +29,9 @@ function CarsPage() {
 
   return (
     <>
-    <div className='CarListPage'>
         <Link to={`/create-car`}><button class="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
             Add Car</button></Link>
+    <div className='CarListPage'>
         {cars.map((el) => {
             return (
                 <div className="Carcard card" key={el._id}>
